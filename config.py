2646272855cdab736fd3604c7594eec199fe9fd3
feroz khan <ferozkhan27@gmail.com>
2020-09-13 01:20:43 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
