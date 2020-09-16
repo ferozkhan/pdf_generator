6c76832fb44495b0e99e6d374de3549c68c00201
feroz khan <ferozkhan27@gmail.com>
2020-09-16 23:18:46 +0200

    add flask test client

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,7 +1,9 @@
 import json
 import os
+import tempfile
 
 import pytest
+from flaskr import flaskr
 
 
 @pytest.fixture
@@ -9,3 +11,17 @@ def invoice_data():
     _dirname = os.path.dirname(__file__)
     with open(f'{_dirname}/data/invoice.json') as f:
         return json.load(f)
+
+
+@pytest.fixture
+def client():
+    _db, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(_db)
+    os.unlink(flaskr.app.config['DATABASE'])
