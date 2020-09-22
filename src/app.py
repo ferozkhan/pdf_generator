e9809202d7afb241d337aae70ad0e10cf4b6e645
feroz khan <ferozkhan27@gmail.com>
2020-09-22 23:12:23 +0200

    return generated pdf as response.

--- src/app.py
+++ src/app.py
@@ -3,15 +3,15 @@ import logging
 import os
 import time
 
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
-import requests
 from weasyprint import HTML
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['CLIENT_PDF'] = os.path.dirname(os.path.realpath(__file__)) + '/client_pdf'
 db = SQLAlchemy(app)
 
 log = logging.getLogger(__name__)
@@ -30,14 +30,14 @@ def home():
     return render_template_string(FORM)
 
 
-def generate_pdf(url):
+def generate_pdf(url, filename):
     """
     generate pdf document
 
     :param url: valid url with http/https
     :return:
     """
-    HTML(url).write_pdf(str(time.time()))
+    HTML(url).write_pdf(filename)
 
 
 @app.route('/pdf')
@@ -52,8 +52,10 @@ def get_pdf():
         log.error("Locations not provided.")
         return render_template_string(FORM, errors="Missing locations.")
     for url in urls:
-        generate_pdf(url)
-    return f"pdf created for {urls}"
+        filename = f"{str(time.time())}.pdf"
+        generate_pdf(url, filename)
+    return send_from_directory(app.config['CLIENT_PDF'],
+                               filename=filename, as_attachment=True)
 
 
 if __name__ == '__main__':
